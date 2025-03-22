03a3197e15f2f721adb3ddf15e8ed39a0d7a43c2
Szasza Palmer <szasza@szasza.info>
2025-03-22 22:22:23 +1100

    fixing Wing LSP execution under Windows
    Signed-off-by: Szasza Palmer <szasza@szasza.info>

--- src/main/kotlin/com/github/wingidea/ide/WingCommandLine.kt
+++ src/main/kotlin/com/github/wingidea/ide/WingCommandLine.kt
@@ -2,6 +2,7 @@ package com.github.wingidea.ide
 
 import com.intellij.execution.ExecutionException
 import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.process.OSProcessHandler
 import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
 import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter
 import com.intellij.javascript.nodejs.interpreter.wsl.WslNodeInterpreter
@@ -21,35 +22,64 @@ class WingCommandLine {
         private fun createCommand(project: Project, vararg commands: String): GeneralCommandLine {
             val interpreter = NodeJsInterpreterManager.getInstance(project).interpreter
             if (interpreter !is NodeJsLocalInterpreter && interpreter !is WslNodeInterpreter) {
-                // shouldn't happen, checked in PrismaLspServerSupportProvider
                 throw ExecutionException("NodeJS/WSL interpreter is not configured for the project, the LSP server cannot be started.")
             }
 
             val npmPath = NpmManager.getInstance(project).getPackage(interpreter)!!.systemDependentPath
-            val nodeBinPath = npmPath.replaceAfterLast("/", "").replaceAfterLast("\\", "").trimEnd('/','\\')
             val cmdLine = GeneralCommandLine("")
-            val envPath = cmdLine.parentEnvironment["PATH"]
+            val nodeModuleBinPath: String
 
             val wingExecutable =
                 if (SystemInfo.isWindows) {
-                    if (cmdLine.parentEnvironment.containsKey("PROMPT")) {
-                        "wing.cmd"
-                    } else {
-                        "wing.ps1"
-                    }
+                    nodeModuleBinPath = getWindowsNodeModuleBinPath(npmPath)
+                    getWindowsExecutable(nodeModuleBinPath)
                 } else {
+                    nodeModuleBinPath = npmPath.replaceAfterLast("/", "").replaceAfterLast("\\", "").trimEnd('/','\\')
                     "wing"
                 }
 
-            return cmdLine.apply {
+            val cmd = cmdLine.apply {
                 withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
                 withEnvironment(parentEnvironment)
-                withEnvironment("PATH", "$envPath:$nodeBinPath")
+                // TODO we need to check if Wing is on the path
                 withCharset(Charsets.UTF_8)
                 withWorkDirectory(project.basePath)
-                withExePath(wingExecutable)
+                withExePath("$nodeModuleBinPath\\$wingExecutable")
                 addParameters(*commands)
             }
+
+            return cmd
+        }
+
+        private fun getWindowsNodeModuleBinPath(npmPath: String): String {
+            val npmModulesBinPathCommandLine = GeneralCommandLine(npmPath)
+            npmModulesBinPathCommandLine.addParameters("config", "get", "prefix")
+            val npmProcessHandler = OSProcessHandler(npmModulesBinPathCommandLine)
+            val bufferedReader = npmProcessHandler.process.inputStream.bufferedReader()
+
+            return bufferedReader.readLine()
+        }
+
+        private fun getWindowsExecutable(nodeModuleBinPath: String): String {
+            val cmdLine = GeneralCommandLine("")
+
+            cmdLine.withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+            cmdLine.withEnvironment(cmdLine.parentEnvironment)
+            val executables = arrayOf("wing.cmd", "wing.ps1", "wing")
+
+            for (executable in executables) {
+                try {
+                    cmdLine.withExePath("$nodeModuleBinPath\\$executable")
+                    val processHandler = OSProcessHandler(cmdLine)
+                    val output = processHandler.process.errorStream.bufferedReader().readLine().split(":")[0]
+
+                    if (output == "Usage") {
+                        return executable
+                    }
+                } catch (_: Exception) {}
+            }
+
+            throw ExecutionException("None of the Winglang executables can be executed. Please check if Wing is installed.")
         }
 
         fun createLsp(project: Project): GeneralCommandLine = createCommand(project, "lsp", "--no-update-check")
